e2203f182fcffe8bcdc5546c56f682b49f381142
Nacho <>
2020-11-24 20:21:03 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./styles/app.scss";
+import Player from "./components/Player";
+import Library from "./components/Library";
+import Song from "./components/Song";
+import data from "./data.js";
+
+function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
+      <Library songs={songs} setCurrentSong={setCurrentSong} />
+    </div>
+  );
+}
+
+export default App;
